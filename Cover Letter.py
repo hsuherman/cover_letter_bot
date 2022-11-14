00f71d794cfed3ef98bfebcd08b30496f9c8b48b
Hans - Thinkpad <stanislaushans.suherman@gmail.com>
2022-11-13 21:22:59 -0800

    v1.1 - Added stuff

--- Cover Letter.py
+++ Cover Letter.py
@@ -4,4 +4,11 @@ Spyder Editor
 
 This is a temporary script file.
 """
+api_key = "test"
 
+import googlemaps
+from googlemaps import GoogleMaps
+gmaps = GoogleMaps(api_key)
+address = 'Constitution Ave NW & 10th St NW, Washington, DC'
+lat, lng = gmaps.address_to_latlng(address)
+print(lat, lng)
